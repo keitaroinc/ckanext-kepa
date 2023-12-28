952fa57a956ee07e027616613dde7e3e71367066
blagoja <blagojabozinovski@gmail.com>
2023-12-28 09:06:05 +0100

    Translation settings

--- ckanext/kepa/plugin.py
+++ ckanext/kepa/plugin.py
@@ -1,10 +1,11 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.lib.plugins import DefaultTranslation
 
 
-class KepaPlugin(plugins.SingletonPlugin):
+class KepaPlugin(plugins.SingletonPlugin, DefaultTranslation):
     plugins.implements(plugins.IConfigurer)
-    
+    plugins.implements(plugins.ITranslation) 
 
     # IConfigurer
 
@@ -12,5 +13,4 @@ class KepaPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "kepa")
-
-    
+   
